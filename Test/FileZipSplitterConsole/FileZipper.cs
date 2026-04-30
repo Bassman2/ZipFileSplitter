b8504502aaa90adb4f43ed29da92ed5a95ef5a97
Ralf Beckers <ralf.beckers@hotmail.com>
2026-04-30 22:07:31 +0200

    Update

---
+++ Test/FileZipSplitterConsole/FileZipper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileZipSplitterConsole;
+
+public class FileZipper
+{
+
+    public int Run()
+    {
+
+        return 0;
+    }
+}
---
+++ Test/FileZipSplitterConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CommandLine;
+
+namespace FileZipSplitterConsole
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            RootCommand rootCommand = new("File Zip Splitter Console");
+
+            Command zipSubCommand = new("zip", "Zip files or folders");
+            zipSubCommand.Add(new Option<FileInfo>("-o", "--output") { Description = "Output folder for unzipped files", Required = true });
+            zipSubCommand.Add(new Option<FileInfo>("-i", "--input") { Description = "Files or folder to zip", AllowMultipleArgumentsPerToken = true, Required = true });
+            rootCommand.Subcommands.Add(zipSubCommand);
+
+            Command unzipCommand = new("unzip", "Unzip to folder");
+            unzipCommand.Add(new Option<FileInfo>("-i", "--input") { Description = "Output folder for unzipped files", Required = true });
+            unzipCommand.Add(new Option<FileInfo>("-o", "--output") { Description = "Output folder for unzipped files", Required = true });
+            rootCommand.Subcommands.Add(unzipCommand);
+                       
+
+            rootCommand.SetAction(parseResult =>
+            {
+                FileZipper zipper = new();
+                ///FileInfo parsedFile = parseResult.GetValue(fileOption);
+                return zipper.Run();
+            });
+
+            ParseResult parseResult = rootCommand.Parse(args);
+            return parseResult.Invoke();
+        }
+    }
+}
