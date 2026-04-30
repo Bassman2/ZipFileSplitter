1f735e620ffcee6fb28ef43e67066b82d48b230a
Ralf Beckers <ralf.beckers@hotmail.com>
2026-04-30 19:01:14 +0200

    Update

---
+++ Src/ZipFileSplitterShare/FileSplitterStream.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.IO.Compression.FileSystem
+{
+    internal class FileSplitterStream : Stream
+    {
+        private FileStream fs;
+        private string path;
+        private int index = 1;
+        private long overallLength = 0;
+        private long splitLength = 0;
+        private long splitSize = 0;
+
+        public FileSplitterStream(string path, long splitSize, FileMode mode, FileAccess access, FileShare share)
+        {
+            this.path = path;
+            this.splitSize = splitSize; 
+            string filePath = $"{path}.{index:03}";
+            fs = new FileStream(filePath, mode, access, share, bufferSize: 0x1000, useAsync: false);
+        }
+
+        public override bool CanRead => fs.CanRead;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => fs.CanWrite;
+
+        public override long Length => overallLength;
+
+        public override long Position
+        {
+            get => overallLength;
+            set => throw new NotSupportedException();
+        }
+
+        public override void Flush() => fs.Flush();
+
+        public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+        
+        public override void SetLength(long value) => throw new NotSupportedException();
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            long c = Math.Min(count, splitSize - splitLength);
+            if (c > 0)
+            {   
+                fs.Write(buffer, offset, count);
+                splitLength += c;
+                overallLength += c;
+                count -= (int)c;
+            }
+            if (count > 0)
+            {
+                fs.Close();
+                
+                index++;
+                string filePath = $"{path}.{index:03}";
+                fs = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write, FileShare.None, bufferSize: 0x1000, useAsync: false);
+                splitLength = 0;
+
+                fs.Write(buffer, (int)(offset + c), count);
+                splitLength += count;
+                overallLength += count;
+            }
+            
+        }
+    }
+}
---
+++ Src/ZipFileSplitterShare/ZipFileSplitter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.IO.Compression.FileSystem;
+
+public static class ZipFileSplitter
+{
+
+    //public static ZipArchive OpenRead(string archiveFileName, long splitSize) => Open(archiveFileName, splitSize, ZipArchiveMode.Read);
+
+    //public static ZipArchive Open(string archiveFileName, long splitSize, ZipArchiveMode mode) => Open(archiveFileName, splitSize, mode, entryNameEncoding: null);
+
+    public static ZipArchive Open(string archiveFileName, ZipArchiveMode mode = ZipArchiveMode.Read, long splitSize = 0, Encoding? entryNameEncoding = null)
+    {
+        // Relies on FileStream's ctor for checking of archiveFileName
+
+        FileMode fileMode;
+        FileAccess access;
+        FileShare fileShare;
+
+        switch (mode)
+        {
+        case ZipArchiveMode.Read:
+            fileMode = FileMode.Open;
+            access = FileAccess.Read;
+            fileShare = FileShare.Read;
+            break;
+
+        case ZipArchiveMode.Create:
+            fileMode = FileMode.CreateNew;
+            access = FileAccess.Write;
+            fileShare = FileShare.None;
+            break;
+
+        case ZipArchiveMode.Update:
+            fileMode = FileMode.OpenOrCreate;
+            access = FileAccess.ReadWrite;
+            fileShare = FileShare.None;
+            break;
+
+        default:
+            throw new ArgumentOutOfRangeException(nameof(mode));
+        }
+
+        // Suppress CA2000: fs gets passed to the new ZipArchive, which stores it internally.
+        // The stream will then be owned by the archive and be disposed when the archive is disposed.
+        // If the ctor completes without throwing, we know fs has been successfully stores in the archive;
+        // If the ctor throws, we need to close it here.
+
+        FileSplitterStream fs = new(archiveFileName, splitSize, fileMode, access, fileShare);
+
+        try
+        {
+            return new ZipArchive(fs, mode, leaveOpen: false, entryNameEncoding: entryNameEncoding);
+        }
+        catch
+        {
+            fs.Dispose();
+            throw;
+        }
+    }
+}
